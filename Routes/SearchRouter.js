57249f84166ddc2c0965ea1193092c58afa0afd2
prabh-ux <prabhkiratsinghbusiness@gmail.com>
2025-07-28 17:21:08 +0530

    Fix import path casing for ElementController

--- Routes/SearchRouter.js
+++ Routes/SearchRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {ElementController} from "../Controllers/elementController.js";
+import {ElementController} from "../Controllers/ElementController.js";
 import {ensureJwtValidation} from "../MiddleWare/jwtVerify.js";
 const router=Router();
 
