c74046d4f6df0879c9939dbec08f6a5667d84fbc
prabh-ux <prabhkiratsinghbusiness@gmail.com>
2025-07-28 20:38:10 +0530

    updated newsletter

--- Controllers/newsLetterController.js
+++ Controllers/newsLetterController.js
@@ -1,39 +1,41 @@
 import nodemailer from 'nodemailer';
 
 
-export const sendMail=async(req,res)=>{
-const {email,name}=req.body;
+export const sendMail = async (req, res) => {
+    const { email, name } = req.body;
 
-if(!email){
-    return res.status(404).json({msg:"email not found"});
-}
+    if (!email) {
+        return res.status(404).json({ msg: "email not found" });
+    }
 
-try {
+    try {
 
-    //transporter
-    const transpoter=nodemailer.createTransport({
+        //transporter
+        const transpoter = nodemailer.createTransport({
 
-        service:"gmail",
-        auth:{
-            user:process.env.EMAIL,
-            pass:process.env.EMAILPASS
-        }
-    });
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.EMAILPASS
+            }
+        });
 
-    //mail options;
-    const mailOptions={
-        from:email,
-        to:process.env.EMAIL,
-        subject:"New Subscriber of DreamScapes",
-        text:`You have a new subscriber with name:${name} and email:${email}`
-    };
+        //mail options;
+        const mailOptions = {
+            from: process.env.EMAIL,
+            to: process.env.EMAIL,
+            replyTo: email,                   // ✅ sets the user's email for replies
 
-    //send mail
-    await transpoter.sendMail(mailOptions);
-    res.status(200).json({msg:"mail sent successfully"});
-    
-} catch (error) {
-    res.status(500).json({msg:"internal server error while subscribing "+error});
-}
+            subject: "New Subscriber of DreamScapes",
+            text: `You have a new subscriber with name:${name} and email:${email}`
+        };
+
+        //send mail
+        await transpoter.sendMail(mailOptions);
+        res.status(200).json({ msg: "mail sent successfully" });
+
+    } catch (error) {
+        res.status(500).json({ msg: "internal server error while subscribing " + error });
+    }
 
 }
